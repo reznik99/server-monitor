5317506a5f922c229a482cfa88be8097f7cbaf35
Francesco <goras.francesco@gmail.com>
2024-12-13 00:07:34 +1300

    Fetch and log logic

--- main.go
+++ main.go
@@ -1,7 +1,53 @@
 package main
 
-import "github.com/sirupsen/logrus"
+import (
+	"github.com/sirupsen/logrus"
+)
+
+var Version = "Development"
 
 func main() {
-	logrus.Infof("Server-Monitor started")
+	logrus.Infof("Server-Monitor %q started", Version)
+
+	// Get OS statistics
+	memStats, err := GetMemoryStats()
+	if err != nil {
+		logrus.Errorf("Failed to get memory statistics: %s", err)
+	}
+	cpuStats, err := GetCPUStats()
+	if err != nil {
+		logrus.Errorf("Failed to get cpu statistics: %s", err)
+	}
+	netStats, err := GetNetworkStats()
+	if err != nil {
+		logrus.Errorf("Failed to get network statistics: %s", err)
+	}
+	tempBoard, err := GetBoardTemp()
+	if err != nil {
+		logrus.Errorf("Failed to get read board temperature: %s", err)
+	}
+	memoryUsagePercent := float32(memStats.Total-memStats.Free) / float32(memStats.Total) * 100
+	cpuUsagePercent := float32(cpuStats.Total-cpuStats.Idle) / float32(cpuStats.Total) * 100
+
+	// Print statistics
+	logrus.Info("Raw stats:")
+	logrus.Infof("- Memory  -> Tot:  %v | Used: %v | Free: %v", HumanFriendlyBytes(memStats.Total), HumanFriendlyBytes(memStats.Used), HumanFriendlyBytes(memStats.Free))
+	logrus.Infof("- CPU     -> Tot:  %v | Idle: %v | System: %v | User: %v", cpuStats.Total, cpuStats.Idle, cpuStats.System, cpuStats.User)
+	logrus.Infof("- Network -> Name: %v | Rx: %v | Tx: %v", netStats[0].Name, HumanFriendlyBytes(netStats[0].RxBytes), HumanFriendlyBytes(netStats[0].TxBytes))
+	logrus.Info("Derived stats:")
+	logrus.Infof("- Memory usage %.2f%%", memoryUsagePercent)
+	logrus.Infof("- CPU usage    %.2f%%", cpuUsagePercent)
+	logrus.Infof("- Temperature  %.2fC", tempBoard)
+
+	// Alerting logic
+	if tempBoard > THRESHOLD_TEMP {
+		logrus.Infof("CPU Temperature %.2f above threshold of %.2f: Sending email alert", tempBoard, THRESHOLD_TEMP)
+		SendEmailAlert()
+	} else if memoryUsagePercent > THRESHOLD_MEM {
+		logrus.Infof("Memory usage %.2f%% above threshold %.2f%%: Sending email alert", memoryUsagePercent, THRESHOLD_MEM)
+		SendEmailAlert()
+	} else if cpuUsagePercent > THRESHOLD_CPU {
+		logrus.Infof("CPU usage %.2f%% above threshold %.2f%%: Sending email alert", cpuUsagePercent, THRESHOLD_CPU)
+		SendEmailAlert()
+	}
 }
