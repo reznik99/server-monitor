703c832775faf702fd2b2a9b17652ea7feb039f5
Francesco <goras.francesco@gmail.com>
2024-12-22 02:17:04 +1300

    Added html email template

---
+++ mail_template.go
@@ -0,0 +1,33 @@
+package main
+
+type alertData struct {
+	Subject            string
+	TempBoard          string
+	MemoryUsagePercent string
+	CPUUsagePercent    string
+	DateTime           string
+	ServerName         string
+	HostName           string
+}
+
+const EmailTemplateStr = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{.Subject}}</title>
+</head>
+<body>
+    <h4>Email alert for <b>{{.ServerName}}</b> <i>[{{.HostName}}]</i></h4>
+	<ul>
+		<li>Board temperature: {{.TempBoard}}</li>
+		<li>Memory Usage: {{.MemoryUsagePercent}}</li>
+		<li>CPU Usage: {{.CPUUsagePercent}}</li>
+		<li>DateTime: {{.DateTime}}</li>
+		<li>ServerName: {{.ServerName}}</li>
+		<li>HostName: {{.HostName}}</li>
+	</ul>
+</body>
+</html>
+`
