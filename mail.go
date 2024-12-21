5c71d2293c3d8e04e215ed3ca750423734348d81
Francesco <goras.francesco@gmail.com>
2024-12-22 02:18:29 +1300

    Use html template, add context timeout to sendmail cmd

--- mail.go
+++ mail.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"html/template"
 	"os"
 	"time"
 
@@ -9,28 +11,43 @@ import (
 )
 
 func SendEmailAlert(tempBoard, memoryUsagePercent, cpuUsagePercent float32) error {
-	var subject = "RaspberrypPI Tor Relay server-monitor alert"
-	var body = fmt.Sprintf(`
-	Board temperature: %.2f%%
-	Memory Usage: %.2f%%
-	CPU Usage: %.2fc
-	Datetime: %s
-	`, tempBoard, memoryUsagePercent, cpuUsagePercent, time.Now().Format(time.RFC822))
-	var fromEmail = os.Getenv("SOURCE_EMAIL_ADDRESS")
-	var toEmail = os.Getenv("TARGET_EMAIL_ADDRESS")
+	// Get email data
+	var ServerName = os.Getenv("SERVER_NAME")
+	var From = os.Getenv("SOURCE_EMAIL_ADDRESS")
+	var To = os.Getenv("TARGET_EMAIL_ADDRESS")
+	var HostName, _ = os.Hostname()
+	var Subject = fmt.Sprintf("%s server-monitor alert", ServerName)
+	var data = alertData{
+		Subject:            Subject,
+		TempBoard:          fmt.Sprintf("%.2fc", tempBoard),
+		MemoryUsagePercent: fmt.Sprintf("%.2f%%", memoryUsagePercent),
+		CPUUsagePercent:    fmt.Sprintf("%.2f%%", cpuUsagePercent),
+		DateTime:           time.Now().Format(time.RFC822),
+		ServerName:         ServerName,
+		HostName:           HostName,
+	}
+	// Parse email template
+	emailTmpl, err := template.New("EmailAlert").Parse(EmailTemplateStr)
+	if err != nil {
+		return fmt.Errorf("failed to parse email template: %s", err)
+	}
 
 	// Create email object
-	// TODO: use HTML template?
 	msg := mail.NewMsg()
-	if err := msg.From(fromEmail); err != nil {
-		return fmt.Errorf("invalid from email address '%s': %s", fromEmail, err)
+	msg.Subject(Subject)
+	if err := msg.From(From); err != nil {
+		return fmt.Errorf("invalid from email address '%s': %s", From, err)
 	}
-	if err := msg.To(toEmail); err != nil {
-		return fmt.Errorf("invalid to email address '%s': %s", toEmail, err)
+	if err := msg.To(To); err != nil {
+		return fmt.Errorf("invalid to email address '%s': %s", To, err)
 	}
-	msg.Subject(subject)
-	msg.SetBodyString(mail.TypeTextPlain, body)
+	if err := msg.SetBodyHTMLTemplate(emailTmpl, data); err != nil {
+		return fmt.Errorf("failed to set email template: %s", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
 
 	// Send email
-	return msg.WriteToSendmail()
+	return msg.WriteToSendmailWithContext(ctx, mail.SendmailPath)
 }
